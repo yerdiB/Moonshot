1266accfee991585d571a8a7a6ad578ed45e2a53
yerdi <yerdi.isman@gmail.com>
2023-01-11 23:37:04 +0600

    first commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Yerdaulet Ismanaliyev on 11.01.2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
